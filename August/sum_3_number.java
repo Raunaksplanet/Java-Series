229f5a3673504d04d8e8df5bf3cb7123070791da
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-01 20:30:39 +0530

    Add files via upload

---
+++ August/sum_3_number.java
@@ -0,0 +1,12 @@
+public class sum_3_number {
+    public static void main(String[] args) {
+
+        int a = 5, b = 4, c = 1;
+        System.out.print("The Sum of three numbers is " + (a + b + c));
+
+        // If we print like this STDOUT will be "The Sum of three numbers is 541",
+        // which is wrong.
+        // System.out.print("The Sum of three numbers is " + a+b+c);
+
+    }
+}
