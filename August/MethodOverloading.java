450d52f70b7d0a64fd7dec0f9fa97811393dff22
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-05 22:19:55 +0530

    Update MethodOverloading.java

--- August/MethodOverloading.java
+++ August/MethodOverloading.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class MethodOverloading {
-    void work(int a,int b){
+    static void work(int a,int b){
         System.out.println("Sum of two number is " + (a+b) + "\n");
     }
-    void work(String str){
+    static void work(String str){
         System.out.println("Hello " + str + ", Good Morning.\n");
     }
-    void work(int f){
+    static void work(int f){
         if(f%2==0) {
             System.out.println("Even\n");
         }
@@ -15,13 +15,12 @@ public class MethodOverloading {
                 System.out.println("Not even\n");
             }
     }
-    void work(String str1, String str2){
+    static void work(String str1, String str2){
         System.out.println(str1 + " is friend of " + str2 + "\n");
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MethodOverloading mo = new MethodOverloading();
         //Method overloading: Two or more methods can have same name but different parameters
         //by using Method Overloading use can simple same method for multiple work.
         int a,b;
@@ -29,25 +28,25 @@ public class MethodOverloading {
 
         System.out.println("1.This Method will Give you some of Two number");
         System.out.println("First Method");
-        System.out.println("Enter 1 number: ");
+        System.out.print("Enter 1 number: ");
         a = sc.nextInt();
-        System.out.println("Enter 2 number: ");
+        System.out.print("Enter 2 number: ");
         b = sc.nextInt();
         sc.nextLine();
-        mo.work(a,b);
+        work(a,b);
 
         System.out.println("2.This Method will Greet you");
         System.out.println("Second Method");
         System.out.print("Enter your name: ");
         str = sc.nextLine();
-        mo.work(str);
+        work(str);
 
         System.out.println("3.This Method will tell you if given number is even or not");
         System.out.println("Third Method");
         System.out.print("Enter a number: ");
         a = sc.nextInt();
         sc.nextLine();
-        mo.work(a);
+        work(a);
 
         System.out.println("4.This Method will tell you something");
         System.out.println("Fourth Method");
@@ -55,6 +54,6 @@ public class MethodOverloading {
         str1 = sc.nextLine();
         System.out.print("Enter Second name: ");
         str2 = sc.nextLine();
-        mo.work(str1,str2);
+        work(str1,str2);
     }
 }
