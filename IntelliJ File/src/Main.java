387987982bb061721b023bedfddd7e8a8ab595b6
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-06 23:47:33 +0530

    Delete IntelliJ File directory

--- IntelliJ File/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-
-        int a = 5,b = 4,c = 1;
-        System.out.print("The Sum of three numbers is " + (a+b+c));
-        }
-    }
