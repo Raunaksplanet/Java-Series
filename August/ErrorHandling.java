e97cf6d4fdee79c961f50098e3bfa780f8952415
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-25 17:39:17 +0530

    Create ErrorHandling.java

---
+++ August/ErrorHandling.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ErrorHandling {
+    public static void main(String[] args) {
+
+        int a = 5;
+        /*
+         * Syntax of try-catch-finally block.
+         * try {
+         *      // Code
+         * } catch(Exception e) {
+         *      System.out.println(e);
+         * } finally {
+         *      // This block will always execute.
+         * }
+         */
+
+        System.out.println("1.Arithematic Exception");
+        // 1.Arithematic Exception
+        try {
+            a = a / 0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("This block will always execute\n");
+        }
+
+
+        System.out.println("2.NullPointerException");
+        // 2.NullPointerException
+        try {
+            String s = null;
+            System.out.println(s.length());
+
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("This block will always execute\n");
+        }
+
+
+        System.out.println("3.NumberFormatException");
+        // 3.NumberFormatException
+        try {
+            String str = "abc";
+            int i = Integer.parseInt(str);
+
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("This block will always execute\n");
+        }
+
+
+        System.out.println("4.ArrayIndexOutOfBoundsException");
+        // 4.ArrayIndexOutOfBoundsException
+        try {
+            int[] arr = new int[3];
+            arr[3] = 10;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("This block will always execute\n");
+        }
+    }
+}
